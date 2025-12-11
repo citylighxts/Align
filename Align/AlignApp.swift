01a8a5107cc1a5bcaac5da0fdfdfbe17fb2e19d5
citylighxts <hanazizahn@gmail.com>
2025-12-11 17:33:42 +0700

    Initial Commit

---
+++ Align/AlignApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlignApp.swift
+//  Align
+//
+//  Created by Hana Azizah on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AlignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlignTests/AlignTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlignTests.swift
+//  AlignTests
+//
+//  Created by Hana Azizah on 11/12/25.
+//
+
+import Testing
+@testable import Align
+
+struct AlignTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
